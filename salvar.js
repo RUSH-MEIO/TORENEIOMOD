555b24da4c69bf731bfef8c6f010822603b88174
Ort4vi0 <82030604+Ort4vi0@users.noreply.github.com>
2025-08-04 11:44:39 -0300

    Add files via upload

---
+++ salvar.js
@@ -0,0 +1,44 @@
+import fs from 'fs';
+export let torneios = [];
+export const DBMASTER = "torneios.json";
+
+export function salvarDados(nomeArquivo, dados, callback) {
+    const jsonString = JSON.stringify(dados, null, 2);
+    fs.writeFile(nomeArquivo, jsonString, (err) => {
+      if (err) {
+        console.log(`Erro ao salvar o arquivo '${nomeArquivo}':`, err);
+      } else {
+        //console.log(`Dados de '${nomeArquivo}' salvos com sucesso!`);
+      }
+      if (callback) callback();
+    });
+  }
+
+export function carregarDados(nomeArquivo, callback) {
+    fs.readFile(nomeArquivo, "utf8", (err, data) => {
+      if (err) {
+        if (err.code === "ENOENT") {
+          console.log(
+            `Arquivo '${nomeArquivo}' não encontrado. Iniciando com uma lista vazia.`
+          );
+          callback([]);
+        } else {
+          console.log(`Erro ao carregar o arquivo '${nomeArquivo}':`, err);
+          callback([]);
+        }
+        return;
+      }
+
+      try {
+        const dados = JSON.parse(data);
+        console.log(`Dados de '${nomeArquivo}' carregados com sucesso.`);
+        callback(dados);
+      } catch (parseErr) {
+        console.log(
+          `Erro ao analisar o JSON do arquivo '${nomeArquivo}':`,
+          parseErr
+        );
+        callback([]);
+      }
+    });
+  }
